ef3609c9d4b285e8ba7307ab37c461fb8b66f0c4
Michael Salton <58754252+michaelsalton@users.noreply.github.com>
2022-07-06 21:12:10 -0400

    Update app.js

--- src/js/app.js
+++ src/js/app.js
@@ -6,7 +6,7 @@ document.addEventListener("DOMContentLoaded", function () {
   particlesJS('particles-js', {
     "particles": {
       "number": {
-        "value": 160,
+        "value": 69,
         "density": {
           "enable": true,
           "value_area": 500
